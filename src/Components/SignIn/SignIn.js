4bac2162e0a019e8174cf893a936f9790ec8fd39
adam.olser <adam.olser@kiwi.com>
2020-04-14 22:55:38 +0200

    update dependecies

--- src/Components/SignIn/SignIn.js
+++ src/Components/SignIn/SignIn.js
@@ -15,7 +15,7 @@ class Signin extends React.Component {
     this.setState({signInPassword: event.target.value})
   }
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/signin', {
+    fetch('https://dry-eyrie-82727.herokuapp.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
